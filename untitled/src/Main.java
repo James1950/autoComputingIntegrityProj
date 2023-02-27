25b9af8ade01c12eb286a53335f97ec0f2e1f8e4
Jake Neiheisel <jneiheisel98@gmail.com>
2023-02-26 20:22:24 -0500

    begin to read in text file

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,9 @@
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        File file = new File("test.txt");
+        File file = new File("untitled/src/test.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
+        ArrayList<String[]> list = new ArrayList<String[]>();
         String st;
         while ((st = br.readLine()) != null)
             System.out.println(st);
