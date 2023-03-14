5b9736cc4dd9e0133117b654183b939ec7b32f6f
jmbau <baumharj@mail.gvsu.edu>
2023-03-13 20:32:41 -0400

    added car Initial_State_of_Vehicle.txt

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         File file = new File("untitled/src/test.txt");
