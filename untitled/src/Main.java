8b6e84c7541457a6a32a9419bcaba6cd03488b54
jmbau <baumharj@mail.gvsu.edu>
2023-03-16 21:42:27 -0400

    notes and start of variables

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        File file = new File("untitled/src/test.txt");
+        System.out.print("This is the initial state of the vehicle:\n");
+        System.out.println();
+        File file = new File("untitled/src/Initial_State_of_Vehicle.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         ArrayList<String[]> list = new ArrayList<String[]>();
         String st;
@@ -11,3 +13,10 @@ public class Main {
             System.out.println(st);
     }
 }
+
+//if we receive a signal from module, look for string in mod to see if it's there; if it is not , reject message
+//if the module is down, look for backup and accept number coming in.
+//if no backup is found, reject number.
+//module status
+//module controller
+// module status -- controls backups
